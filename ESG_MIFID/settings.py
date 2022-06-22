99236669666fa2edde71d73ac9ea19ac1cbd1481
Snehal13Nag <sbhanje2@gmail.com>
2022-06-22 11:04:28 +0530

    ESG_Screenings

--- ESG_MIFID/settings.py
+++ ESG_MIFID/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5u&&smgms$p*ai)6+*9e(z)e$qtkdhefl#sz_ysqq2el4qzv_y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+LOGIN_REDIRECT_URL = 'index'
+HOME_URL = 'index'
+LOGIN_URL = 'login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
